56daeae5bf78c26c11ab28b6378fbcc19e9bf16d
Brayden Nykamp <braynyka385@outlook.com>
2020-11-24 10:28:10 -0500

    Add project files.

---
+++ PlayGame/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PlayGame
+{
+    partial class connorSimWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playButton = new System.Windows.Forms.Button();
+            this.counterLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.playButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Yellow;
+            this.playButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.ForeColor = System.Drawing.Color.Maroon;
+            this.playButton.Location = new System.Drawing.Point(333, 321);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(107, 43);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "Play Game";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // counterLabel
+            // 
+            this.counterLabel.AutoSize = true;
+            this.counterLabel.BackColor = System.Drawing.Color.Black;
+            this.counterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.counterLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.counterLabel.Location = new System.Drawing.Point(336, 145);
+            this.counterLabel.Name = "counterLabel";
+            this.counterLabel.Size = new System.Drawing.Size(274, 36);
+            this.counterLabel.TabIndex = 1;
+            this.counterLabel.Text = "Game will begin in: ";
+            // 
+            // connorSimWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.counterLabel);
+            this.Controls.Add(this.playButton);
+            this.Name = "connorSimWindow";
+            this.Text = "Connor Simulator 2021";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label counterLabel;
+    }
+}
+
---
+++ PlayGame/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace PlayGame
+{
+    public partial class connorSimWindow : Form
+    {
+        public connorSimWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer player = new SoundPlayer(Properties.Resources.Beep);
+            int count = 3;
+            while (count >= 1)
+            {
+
+                playButton.ForeColor = Color.Black;
+                playButton.FlatAppearance.MouseOverBackColor = Color.Black;
+                playButton.FlatAppearance.MouseDownBackColor = Color.Black;
+                playButton.FlatAppearance.BorderColor = Color.Black;
+                playButton.BackColor = Color.Black;
+                counterLabel.ForeColor = Color.White;
+                counterLabel.Text = ("Game will begin in: " + count);
+                count--;
+                player.Play();
+                Refresh();
+                Thread.Sleep(1000);
+            }
+            playButton.ForeColor = Color.Green;
+            playButton.FlatAppearance.MouseOverBackColor = Color.Green;
+            playButton.FlatAppearance.MouseDownBackColor = Color.Green;
+            playButton.FlatAppearance.BorderColor = Color.Green;
+            playButton.BackColor = Color.Green;
+            counterLabel.Text = "GO!!!!!!!!!";
+                counterLabel.BackColor = Color.Green;
+                this.BackColor = Color.Green;
+        }
+    }
+}
